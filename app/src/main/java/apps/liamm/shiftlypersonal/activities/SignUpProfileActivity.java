4b318afa59d024c1c5af4101282b578c28ddf4ca
Liam MacPherson <liam.macpherson@servelec.co.uk>
2019-11-26 19:57:47 +0000

    Add action bar back button functionality

--- app/src/main/java/apps/liamm/shiftlypersonal/activities/SignUpProfileActivity.java
+++ app/src/main/java/apps/liamm/shiftlypersonal/activities/SignUpProfileActivity.java
@@ -2,6 +2,8 @@ package apps.liamm.shiftlypersonal.activities;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+
 import apps.liamm.shiftlypersonal.R;
 
 public class SignUpProfileActivity extends BaseActivity {
@@ -10,5 +12,13 @@ public class SignUpProfileActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar actionBar = getSupportActionBar();
+
+        // Enable the Up button
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
 }
