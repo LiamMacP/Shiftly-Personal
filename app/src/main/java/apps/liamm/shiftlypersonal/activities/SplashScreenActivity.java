512d6d11fa77ee1ada783ce4ecc622a0a6879300
Liam MacPherson <liam.macpherson@servelec.co.uk>
2019-11-26 19:04:47 +0000

    SplashScreenActivity now checks whether user is logged in, if not directs to SignInActivity, else DashboardActivity.

--- app/src/main/java/apps/liamm/shiftlypersonal/activities/SplashScreenActivity.java
+++ app/src/main/java/apps/liamm/shiftlypersonal/activities/SplashScreenActivity.java
@@ -2,16 +2,44 @@ package apps.liamm.shiftlypersonal.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import apps.liamm.shiftlypersonal.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static final String TAG = SplashScreenActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        Intent nextLocationIntent;
+        if (currentUser != null) {
+            nextLocationIntent =
+                    new Intent(SplashScreenActivity.this, DashboardActivity.class);
+        } else {
+            nextLocationIntent =
+                    new Intent(SplashScreenActivity.this, SignInActivity.class);
+        }
+
+        startActivity(nextLocationIntent);
     }
 
     @Override
