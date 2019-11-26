7ee5ad3ed2f810eaea6eaa95458bba7e4c95dde1
Liam MacPherson <liam.macpherson@servelec.co.uk>
2019-11-26 22:15:30 +0000

    Add SetWorkplaceActivity to allow for sign in check.

---
+++ app/src/main/java/apps/liamm/shiftlypersonal/activities/SetWorkplaceActivity.java
@@ -0,0 +1,16 @@
+package apps.liamm.shiftlypersonal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import apps.liamm.shiftlypersonal.R;
+
+public class SetWorkplaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_workplace);
+    }
+}
