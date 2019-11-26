a5629003bce3e5295aea1a4e7d35484b7a6b7f31
Liam MacPherson <liam.macpherson@servelec.co.uk>
2019-11-26 18:10:54 +0000

    Add splash screen activity.

---
+++ app/src/main/java/apps/liamm/shiftlypersonal/activities/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package apps.liamm.shiftlypersonal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import apps.liamm.shiftlypersonal.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+}
